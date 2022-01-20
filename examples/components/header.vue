9a90796d318469bb92f5fd42726e30db2ee3753f
iq9891 <649076408@qq.com>
2022-01-20 18:48:09 +0800

    docs($version): add next version

--- examples/components/header.vue
+++ examples/components/header.vue
@@ -28,9 +28,20 @@
             <span class="gh-text">Star</span>
           </a>
         </span>
-        <a style="float:right;margin: 20px 0 0 64px;" href="https://www.npmjs.org/package/@fe6/water-pro" target="_blank">
+        <a style="float:right;margin: 20px 0 0 32px;" href="https://www.npmjs.org/package/@fe6/water-pro" target="_blank">
           <img src="https://img.shields.io/npm/v/@fe6/water-pro.svg?style=flat-square" />
         </a>
+        <a-select
+          style="float:right;margin: 20px 0 0 24px;"
+          key="version"
+          class="version"
+          size="small"
+          :default-value="antdVersion"
+          :get-popup-container="(trigger) => trigger.parentNode"
+        >
+          <a-select-option value="4.x" @click="changeVersion">4.x</a-select-option>
+          <a-select-option :value="antdVersion">{{ antdVersion }}</a-select-option>
+        </a-select>
         <a-menu :selectedKeys="selectedKeys" mode="horizontal" class="menu-site" id="nav">
           <a-menu-item key="components">
             <router-link to="/components">组件</router-link>
@@ -41,11 +52,19 @@
         </a-menu>
       </a-col>
     </a-row>
+    <div class="global-notification">
+      <span> v4 版本已发布，请访问 &nbsp;&nbsp; <a href="http://water-v4.chjgo.com" target="_blank">water-v4.chjgo.com</a> &nbsp;&nbsp;查看更多详情 </span>
+      <span tabindex="-1" role="img" aria-label="close" class="anticon anticon-close close-icon" style="position: absolute; top: 8px; right: 15px;">
+        <svg focusable="false" class="" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true" viewBox="64 64 896 896"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path>
+        </svg>
+      </span>
+    </div>
   </header>
 </template>
 
 <script>
 import { ref, unref, computed, watch } from 'vue';
+import { version } from '@fe6/water-pro';
 import { useRouter } from 'vue-router';
 import { hyphenate } from '@fe6/shared';
 import router from '../routers';
@@ -101,7 +120,13 @@ export default {
       selPlaceHolder.value = selDefPlaceHolder[newPageTrueType];
       selOpts.value = options(newPageTrueType);
     })
+    const antdVersion = ref(version);
+    const changeVersion = () => {
+      location.href = 'http://water-v4.chjgo.com';
+    };
     return {
+      antdVersion,
+      changeVersion,
       selectedKeys,
       searchValue,
       selPlaceHolder,
