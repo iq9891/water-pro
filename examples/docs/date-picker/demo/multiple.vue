98e59bd1bd4e0f5290269272dfc27d4fc495aaa9
天意 <649076408@qq.com>
2022-06-21 10:47:23 +0800

    fix($taggroup): style error in popover

--- examples/docs/date-picker/demo/multiple.vue
+++ examples/docs/date-picker/demo/multiple.vue
@@ -1,8 +1,8 @@
 <template>
   <a-space direction="vertical">
     <a-date-picker v-model:value="valueDate" type="multiple" />
-    <a-month-picker v-model:value="valueMonth" type="multiple" />
-    <a-year-picker v-model:value="valueYear" type="multiple" />
+    <a-month-picker v-model:value="valueMonth" type="multiple" multipleTagGroupPopoverClass="monthmore" />
+    <a-year-picker v-model:value="valueYear" type="multiple" multipleTagGroupPopoverClass="yearmore" />
   </a-space>
 </template>
 <script lang="ts">
@@ -11,10 +11,19 @@ import moment, { Moment } from 'moment';
 export default defineComponent({
   setup() {
     return {
-      valueYear: ref<Moment[]>([moment()]),
-      valueMonth: ref<Moment[]>([moment()]),
-      valueDate: ref<Moment[]>([moment()]),
+      valueYear: ref<Moment[]>([moment(), moment('2021-01-10'), moment('2012-01-11')]),
+      valueMonth: ref<Moment[]>([moment(), moment('2021-01-10'), moment('2012-01-11')]),
+      valueDate: ref<Moment[]>([moment(), moment('2021-01-10'), moment('2012-01-11')]),
     };
   },
 });
 </script>
+
+<style lang="less">
+.yearmore {
+  width: 129px;
+}
+.monthmore {
+  width: 167px;
+}
+</style>
