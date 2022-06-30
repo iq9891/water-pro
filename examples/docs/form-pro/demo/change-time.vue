8492fb3762784a2c8d8d0280ecc3461039bdfe83
天意 <649076408@qq.com>
2022-06-30 22:16:54 +0800

    fix($datepicker): display error

--- examples/docs/form-pro/demo/change-time.vue
+++ examples/docs/form-pro/demo/change-time.vue
@@ -16,88 +16,13 @@ const schemas: FormSchema[] =[
       valueFormat: 'YYYY-MM-DD',
       format: 'YYYY年MM月DD日',
       multipleMaxTagTextLength: 15,
-      multipleMaxTagCount: 4,
+      // multipleMaxTagCount: 4,
       multipleTagGroupPopoverClass: 'test1',
       disabledDate: (current: Moment) => {
         return current && current < moment().endOf('day');
       },
     },
     label: '多选日期',
-  },
-  {
-    field: 'mortmonth',
-    component: 'MonthPicker',
-    componentProps: {
-      type: 'multiple',
-      multipleMaxTagCount: 2,
-      valueFormat: 'YYYY-MM',
-    },
-    label: '多选月份',
-  },
-  {
-    field: 'mortyear',
-    component: 'YearPicker',
-    componentProps: {
-      type: 'multiple',
-      valueFormat: 'YYYY',
-    },
-    label: '多选年份',
-  },
-  {
-    field: 'date',
-    component: 'DatePicker',
-    label: '日期',
-  },
-  {
-    field: 'month',
-    component: 'MonthPicker',
-    label: '月份',
-  },
-  {
-    field: 'year',
-    component: 'YearPicker',
-    label: '年份',
-  },
-  {
-    field: 'week',
-    component: 'WeekPicker',
-    label: '周',
-  },
-  {
-    field: 'time',
-    component: 'TimePicker',
-    label: '时间',
-  },
-  {
-    field: 'rangeDate',
-    component: 'RangePicker',
-    label: '日期区间',
-  },
-  {
-    field: 'rangeDateTime',
-    component: 'RangePicker',
-    label: '日期时间区间',
-    componentProps: {
-      showTime: true,
-    }
-  },
-  {
-    field: 'rangeGroupPicker',
-    component: 'RangeGroupPicker',
-    label: '日期区间快捷',
-    componentProps: {
-      showTime: true,
-      timeRounding: true,
-      showTodayButton: true,
-      showYesterdayButton: true,
-      showSevenDaysButton: true,
-      showThirtyDaysButton: true,
-    }
-  },
-  {
-    field: 'rangeTime',
-    component: 'TimeRangePicker',
-    label: '时间区间',
   },
 ];
 
@@ -124,7 +49,7 @@ export default defineComponent({
 
     onMounted(() => {
       setFieldsValue({
-        mortdate: [moment().subtract(3, 'd')],
+        mortdate: [moment().subtract(3, 'd'), moment().add(3, 'd')],
         mortmonth: [moment().subtract(3, 'd')],
       })
     })
